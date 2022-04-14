151ac000ebfae6718482f70a250c8058dafd8d02
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2022-04-13 22:28:13 -0300

    complete DOM

---
+++ 143/console-command.js
@@ -0,0 +1,4 @@
+document.querySelector("button"); // returns the first match
+document.querySelectorAll("button"); // returns a list of all items
+// query selector more often used than get element methods
+// .style.color = "blue";
---
+++ 144/console-command.js
@@ -0,0 +1,4 @@
+document.querySelector("button").style.backgroundColor = "yellow";
+// .classList.add()
+// .classList.remove()
+// .classList.toggle() turn it on or off
