3ff3018d63691253b8013e15d3007b9a70fc3f0c
Leo Hong <leo.hong@unb.ca>
2022-04-21 23:55:25 -0300

    finish section 27

--- S27-FruitsProjectMongoose/app.js
+++ S27-FruitsProjectMongoose/app.js
@@ -1,10 +1,24 @@
+/*
+    Connect 
+*/
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fruitsDB')
 
+/*
+    Create
+*/
 const fruitSchema = new mongoose.Schema(
-    {
-        name: String,
-        rating: Number,
+    {   
+        // validation
+        name: {
+            type: String,
+            required: [true, "Name is required."]
+        },
+        rating: { 
+            type: Number,
+            min: 1,
+            max: 10
+        },
         review: String
     }
 );
@@ -19,7 +33,6 @@ const fruit = new Fruit({
 // save to collection
 // fruit.save();
 
-
 const kiwi = new Fruit({
     name: "Kiwi",
     rating: 7,
@@ -33,26 +46,85 @@ const banana = new Fruit({
 });
 
 // insert many
-Fruit.insertMany([kiwi, banana], function(err){
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Successfully saved!");
-    }
-});
+// Fruit.insertMany([kiwi, banana], function(err){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully saved!");
+//     }
+// });
 
 const personSchema = new mongoose.Schema(
     {
         name: String,
         age: Number,
+        favouriteFruit: fruitSchema // relationship
     }
-); 
-const Person = mongoose.model("Person", personSchema);// a collection called "people" in fruitsDB will be created
+);
 
+const Person = mongoose.model("Person", personSchema);// a collection called "people" in fruitsDB will be created
 
 const person = new Person({
     name: "John",
-    age: 37
+    age: 37,
+    favouriteFruit: banana
 });
 
-//person.save();
+// person.save();
+
+/*
+    Read
+*/
+Fruit.find(function(err, fruits){
+    if (err){
+        console.log(err);
+    } else {
+        console.log(fruits);
+    }
+
+});
+
+Fruit.find(function(err, fruits){
+    if (err){
+        console.log(err);
+    } else {
+        fruits.forEach(function(fruit){
+            console.log(fruit.name);
+        });
+    }
+
+});
+
+/*
+    Update
+*/
+Fruit.updateOne({_id: "62620d260f9828e0726d8efb"}, {name: "Peach"}, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        // mongoose.connection.close(); // close the connection
+        console.log("Successfully updated the document");
+    }
+});
+
+/*
+    Delete
+*/
+Fruit.deleteOne({name: "Peach"}, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        // mongoose.connection.close(); // close the connection
+        console.log("Successfully deleted the document");
+    }
+});
+
+Person.deleteMany({name: "John"}, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        mongoose.connection.close(); // close the connection
+        console.log("Successfully deleted the document");
+    }
+});
+
--- notes.md
+++ notes.md
@@ -6,3 +6,4 @@ Objects & callbacks: 163, 166, 168, 171
 
 # MongoDB
 This section (26) is outdated. Should try freeCodeCamp or The Odin Project later. 
+MongoDB issue: https://stackoverflow.com/questions/37565758/mongodb-not-working-on-ubuntu-mongod-service-failed-with-result-exit-code
